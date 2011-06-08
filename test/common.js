29a270bbd489c9612375eac288a30ccfdb73b632
James E. Marca <jmarca@translab.its.uci.edu>
2011-06-08 13:12:04 -0700

    added check of environment variables for running tests

--- test/common.js
+++ test/common.js
@@ -1,3 +1,9 @@
+var env = process.env;
+var user = process.env.COUCHDB_USER || '';
+var pass = process.env.COUCHDB_PASS || '' ;
+var host = process.env.COUCHDB_HOST || '127.0.0.1' ;
+var port = process.env.COUCHDB_PORT  || 5984;
+
 var sys = require('sys');
 global.p = sys.p;
 global.puts = sys.puts;
@@ -11,4 +17,4 @@ global.checkCallbacks = function(callbacks) {
 };
 
 // Provide a port/host here if your local db has a non-default setup
-GLOBAL.client = couchdb.createClient();
+GLOBAL.client = couchdb.createClient(port,host,user,pass);
